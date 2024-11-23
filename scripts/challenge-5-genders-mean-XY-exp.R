ace4bcc7fdbc1562e8122b325fb753cb6c5b6ee5
h-khabaz <108627398+h-khabaz@users.noreply.github.com>
2024-11-23 16:39:46 +0000

    Updated Challenge 5.1 solution to export results.

--- scripts/challenge-5-genders-mean-XY-exp.R
+++ scripts/challenge-5-genders-mean-XY-exp.R
@@ -1,5 +1,5 @@
 library(tidyverse)
 rna <- read_csv("data/raw/rnaseq.csv")
 solution <- rna %>% select(chromosome_name,sex,expression) %>%filter(chromosome_name=="X"|chromosome_name=="Y") %>% group_by(chromosome_name,sex) %>% summarise(mean_expression=mean(expression)) %>% pivot_wider(names_from = sex,values_from = mean_expression)
-
+write_csv(solution,"data/processed/5.1.genders-mean-XY-exp-output.csv")
 
